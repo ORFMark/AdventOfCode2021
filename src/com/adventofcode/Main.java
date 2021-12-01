8203b628dd910937a233d7f5ec8bb468cc287bcb
ORFMark <markrb0609@gmail.com>
2021-12-01 08:59:59 -0800

    Inital Commit

---
+++ src/com/adventofcode/Main.java
@@ -0,0 +1,52 @@
+package com.adventofcode;
+
+import problems.Problem;
+import problems.dayone.SonarSweepPart1;
+import problems.dayone.SonarSweepPart2;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	List<Problem> problemList = new LinkedList<>();
+    List<String> inputList = new LinkedList<>();
+    try {
+        inputList.add(readInputFromFile("ProblemInput/Day1SonarSweep/SonarSweep.txt"));
+        inputList.add(readInputFromFile("ProblemInput/Day1SonarSweep/SonarSweep.txt"));
+    } catch (Exception e) {
+        System.out.println("Invalid File Path! Can't read input");
+        return;
+    }
+    problemList.add(new SonarSweepPart1());
+    problemList.add(new SonarSweepPart2());
+
+    int dayNumber = 0;
+    for (Problem prob : problemList) {
+        System.out.printf("Day %d Part %d: ", (dayNumber/2) +1, (dayNumber % 2) + 1);
+        System.out.println(prob.run(inputList.get(0)));
+
+        inputList.remove(0);
+        dayNumber++;
+    }
+
+    }
+
+    public static String readInputFromFile(String filePath) throws IOException {
+        FileInputStream in = new FileInputStream(filePath);
+        String input = null;
+        try {
+            input = new String(in.readAllBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            in.close();
+        }
+
+        return input;
+    }
+}
+
